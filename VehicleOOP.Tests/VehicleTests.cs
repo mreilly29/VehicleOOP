81b0dcc6c0e0d7920029ba0142e9d17c58c14d62
mreilly29 <mreilly18@jcu.edu>
2018-10-14 13:12:59 -0400

    first commit

---
+++ VehicleOOP.Tests/VehicleTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleOOP.Tests
+{
+    [TestFixture]
+    public class VehicleTests
+    {
+        Car car = new Car();
+        [Test]
+        public void Create_Veh_CTR_1_Input()
+        {
+            Car car = new Car("Ford");
+            string response = car.Make;
+            Assert.That(response, Is.EqualTo("Ford"));
+        }
+
+        [Test]
+        public void Create_Veh_CTR_2_Input()
+        {
+            Car car = new Car("Ford", "Bronco");
+            string response = car.Model;
+            Assert.That(response, Is.EqualTo("Bronco"));
+        }
+
+        [Test]
+        public void Create_Veh_CTR_3_Input()
+        {
+            Car car = new Car("Ford", "Bronco", "1965");
+            string response = car.Year;
+            Assert.That(response, Is.EqualTo("1965"));
+        }
+
+        [Test]
+        public void Set_Engine_Size()
+        {
+            car.EngSize = 5.0;
+            double response = car.EngSize;
+            Assert.That(response, Is.EqualTo(5.0));
+        }
+
+        [Test]
+        public void Set_Rim_Size()
+        {
+            car.RimSize = 16;
+            int response = car.RimSize;
+            Assert.That(response, Is.EqualTo(16));
+        }
+
+        [Test]
+        public void Set_Veh_Color()
+        {
+            car.Color = "Red";
+            string response = car.Color;
+            Assert.That(response, Is.EqualTo("Red"));
+        }
+
+        [Test]
+        public void Get_MPH()
+        {
+            string response = car.GetMPH(1,7) + " MPG";
+            Assert.That(response, Is.EqualTo("9 MPG"));
+        }
+                
+        [Test]
+        public void Get_MPG()
+        {
+            //car.GetMPG(3117, 3579, 9251);
+            string response = car.GetMPG(3117, 3579, 9251);
+            Assert.That(response, Is.EqualTo("49.94 MPG"));
+        }
+    }
+}
---
+++ VehicleOOP/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleOOP
+{
+    //Maggie Reilly 09/19/2018
+    public class Car
+    {
+        //FIELDS
+        private string make;
+        private string model;
+        private string year;
+        private double engSize;
+        private int rimSize;
+        private string color;
+
+        //PROPERTIES
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public string Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public double EngSize
+        {
+            get { return this.engSize; }
+            set { this.engSize = value; }
+        }
+
+        public int RimSize
+        {
+            get { return this.rimSize; }
+            set { this.rimSize = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        //CONSTRUCTORS
+        public Car()
+        {
+
+        }
+
+        public Car(string carMake)
+        {
+            this.Make = carMake;
+        }
+
+        public Car(string carMake, string carModel )
+        {
+            this.Make = carMake;
+            this.Model= carModel;
+        }
+        public Car(string carMake, string carModel, string carYear)
+        {
+            this.Make = carMake;
+            this.Model = carModel;
+            this.Year = carYear;
+        }
+
+        //METHODS
+        public int GetMPH(double distance, double time)
+        {
+           int mph = (int)Math.Round(distance / (time / 60));
+            return mph;
+        }
+
+        public string GetMPG(double endOdo, double startOdo, double tankSize)
+        {
+            string mpg = Math.Round(((startOdo - endOdo)*1000 / tankSize),2) +" MPG";
+            return mpg;          
+        }
+    }
+}
